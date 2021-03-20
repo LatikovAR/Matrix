7c19396177bb4cd4a12ccfc69e6fc87a862f69c4
alexander <sslunatik@mail.ru>
2021-03-20 18:18:24 +0300

    some fixes

--- mem_storage/mem_storage.h
+++ mem_storage/mem_storage.h
@@ -9,8 +9,8 @@ namespace mem_storage {
 template<typename T> class Matrix_Storage;
 
 template <typename T>
-class Vector_Storage {
-protected:
+class Vector_Storage final {
+public:
     T *data_;
     size_t size_;
     size_t used_;
@@ -21,7 +21,7 @@ protected:
         }
     }
 
-    virtual ~Vector_Storage() {
+    ~Vector_Storage() {
         if(size_ == 0) return;
         for(size_t i = 0; i < used_; ++i) {
             data_[i].~T();
@@ -36,9 +36,17 @@ protected:
         data_ = rhs.data_;
         rhs.data_ = nullptr;
         size_ = rhs.size_;
-        rhs.size_ = 0; //is need?
+        rhs.size_ = 0;
         used_ = rhs.used_;
-        rhs.used_ = 0; //is need?
+        rhs.used_ = 0;
+    }
+
+    T& operator()(size_t i)& {
+        return data_[i];
+    }
+
+    const T& operator()(size_t i) const& {
+        return data_[i];
     }
 
     void swap(Vector_Storage& rhs) noexcept {
@@ -50,91 +58,119 @@ protected:
 
 
 template <typename T>
-class Matrix_Storage:
-        protected Vector_Storage<T*>,
-        protected Vector_Storage<T>
-{
+class Matrix_Storage final {
 private:
-    const size_t& mem_size_ = Vector_Storage<T>::size_;
-protected:
-    const size_t& column_size_ = Vector_Storage<T*>::size_;
+    Vector_Storage<T*> rows_;
+    Vector_Storage<T> data_;
     size_t row_size_;
-    using Vector_Storage<T*>::data_;
-    using Vector_Storage<T>::used_;
+
+public:
+    size_t mem_size() const { return data_.size_; }
+    size_t column_size() const { return rows_.size_; }
+    size_t row_size() const { return row_size_; }
+    size_t& used()& { return data_.used_; }
+    T& operator()(size_t row_num, size_t column_num)& {
+        return *(rows_(row_num) + column_num);
+    }
+    const T& operator()(size_t row_num, size_t column_num) const& {
+        return *(rows_(row_num) + column_num);
+    }
+    T*& operator()(size_t row_num)& {
+        return rows_(row_num);
+    }
+    const T*& operator()(size_t row_num) const& {
+        return rows_(row_num);
+    }
 
     Matrix_Storage(size_t column_size, size_t row_size):
-        Vector_Storage<T*>(column_size),
-        Vector_Storage<T>(column_size * row_size),
+        rows_(column_size),
+        data_(column_size * row_size),
         row_size_(row_size)
     {
         for(size_t i = 0; i < column_size; ++i) {
-            data_[i] = &(Vector_Storage<T>::data_[i * row_size]);
-            Vector_Storage<T*>::used_ += 1;
+            rows_(i) = &(data_(i * row_size));
+            rows_.used_ += 1;
         }
     }
 
-    virtual ~Matrix_Storage() = default;
+    ~Matrix_Storage() = default;
 
     Matrix_Storage(const Matrix_Storage<T>&) = delete;
     Matrix_Storage& operator=(const Matrix_Storage<T>&) = delete;
 
     Matrix_Storage(Matrix_Storage&& rhs) noexcept:
-        Vector_Storage<T*>(std::move(rhs.Vector_Storage)),
-        Vector_Storage<T>(std::move(rhs.Vector_Storage))
+        rows_{std::move(rhs.rows_)},
+        data_{std::move(rhs.data_)},
+        row_size_(rhs.row_size_)
     {
-        row_size_ = rhs.row_size_;
-        rhs.row_size_ = 0; //is need?
+        rhs.row_size_ = 0;
     }
 
     void swap(Matrix_Storage<T>& rhs) noexcept {
-        Vector_Storage<T>::swap(rhs);
-        Vector_Storage<T*>::swap(rhs);
+        data_.swap(rhs.data_);
+        rows_.swap(rhs.rows_);
         std::swap(row_size_, rhs.row_size_);
     }
 };
 
 
 template <typename T>
-class Symmetric_Matrix_Storage:
-        protected Vector_Storage<T*>,
-        protected Vector_Storage<T>
-{
+class Symmetric_Matrix_Storage final {
 private:
+    Vector_Storage<T*> rows_;
+    Vector_Storage<T> data_;
+
     static size_t calculate_mem_size(size_t str_size) {
         return ((1 + str_size) * str_size) / 2;
     }
 
-    const size_t& mem_size_ = Vector_Storage<T>::size_;
-protected:
-    using Vector_Storage<T*>::size_;
-    using Vector_Storage<T*>::data_;
-    using Vector_Storage<T>::used_;
+public:
+    size_t mem_size() const { return data_.size_; }
+    size_t size() const { return rows_.size_; }
+    size_t& used()& { return data_.used_; }
+    T& operator()(size_t row_num, size_t column_num)& {
+        if(row_num >= column_num) {
+            return *(rows_(row_num) + column_num);
+        }
+        else {
+            return *(rows_(column_num) + row_num);
+        }
+    }
+    const T& operator()(size_t row_num, size_t column_num) const& {
+        if(row_num >= column_num) {
+            return *(rows_(row_num) + column_num);
+        }
+        else {
+            return *(rows_(column_num) + row_num);
+        }
+    }
 
     Symmetric_Matrix_Storage(size_t size):
-        Vector_Storage<T*>(size),
-        Vector_Storage<T>(calculate_mem_size(size))
+        rows_(size),
+        data_(calculate_mem_size(size))
     {
         size_t p_pos = 0;
         for(size_t i = 0; i < size; ++i) {
-            data_[i] = &(Vector_Storage<T>::data_[p_pos]);
-            Vector_Storage<T*>::used_ += 1;
+            rows_(i) = &(data_(p_pos));
+            rows_.used_ += 1;
 
             p_pos += (i + 1);
         }
     }
 
-    virtual ~Symmetric_Matrix_Storage() = default;
+    ~Symmetric_Matrix_Storage() = default;
 
     Symmetric_Matrix_Storage(const Symmetric_Matrix_Storage<T>&) = delete;
     Symmetric_Matrix_Storage& operator=(const Symmetric_Matrix_Storage<T>&) = delete;
 
     Symmetric_Matrix_Storage(Symmetric_Matrix_Storage&& rhs) noexcept:
-        Vector_Storage<T*>(std::move(rhs.Vector_Storage)),
-        Vector_Storage<T>(std::move(rhs.Vector_Storage)) {}
+        rows_{std::move(rhs.rows_)},
+        data_{std::move(rhs.data_)} {}
 
     void swap(Symmetric_Matrix_Storage<T>& rhs) noexcept {
-        Vector_Storage<T>::swap(rhs);
-        Vector_Storage<T*>::swap(rhs);
+        data_.swap(rhs.data_);
+        rows_.swap(rhs.rows_);
     }
 };
-}
+
+} //namespace mem_storage
